faad143b22e63aa5d836fff308b713b10f35fed6
Hanen Riguen <rh.hanen@gmail.com>
2025-08-18 10:40:53 +0200

    end

--- src/models/adherentModel.js
+++ src/models/adherentModel.js
@@ -4,7 +4,7 @@ const adherentSchema = new mongoose.Schema(
     nom: { type: String, required: true },
     prenom: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    telephone: { type: String, required: true },
+    telephone: { type: String, required: true, unique: true },
     dateInscription: { type: Date, default: Date.now },
     actif: { type: Boolean, default: true }
   },
